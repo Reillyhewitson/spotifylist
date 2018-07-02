5a50452494e65cb77ff58b11ff374257ee1fa28e
Unknown <reilly.hewitson@utcreading.org.uk>
2018-07-02 22:52:19 +0100

    Mostly done
    NB: includes Spotipy no dependencies required

---
+++ app.py
@@ -0,0 +1,42 @@
+import spotipy
+import spotipy.util as util
+import requests
+import json
+from multiprocessing import Pool
+pool = Pool()
+username = 'Reilly Hewitson'
+def list_playlists(x, y,):
+    number = -1
+    for i in results['items']:
+        number += 1
+        print(number,results['items'][number]['name'])
+    playlist = int(input("select playlist"))
+    return playlist
+def list_songs(results, playlist, number, numreal, offset):
+    while number == 99:
+        trackrequest = requests.get('http://api.spotify.com/v1/users/'+username+'/playlists/'+results['items'][playlist]['id']+'/tracks', params = {'offset' : offset},  headers = {"Content-Type":"application/json", 'Authorization':"Bearer "+token})
+        playresult = trackrequest.json()
+        number = -1
+        offset += 100
+        for i in playresult['items']:
+            number += 1
+            numreal += 1
+            print(numreal, playresult['items'][number]['track']['name'])
+while True:
+    '''
+    print('1. List songs \n2. Play a song')
+    select = str(input('What do you want to do? '))
+    result = []
+    if select == '1':'''
+    token = util.prompt_for_user_token('Reilly Hewitson','playlist-read-private',client_id='723d300829e842f8abb20a1d9dc8f80d',client_secret='23901c4170be4a27bc907e656797c640',redirect_uri='http://localhost')
+    sp = spotipy.Spotify(auth=token)
+    request = requests.get('https://api.spotify.com/v1/me/playlists' , headers = {'Authorization': 'Bearer ' + token})
+    results = request.json()
+    playlist = list_playlists(request, results)
+    number = 99
+    offset = 0
+    numreal = 0
+    print('-------------------------------------------------')
+    list_songs(results, playlist, number, numreal, offset)
+    print('-------------------------------------------------')
+
